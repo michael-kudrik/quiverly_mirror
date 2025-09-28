ea6e746a2b241e417ccbe356489699a7a584e9e6
michael-kudrik <mikethesurfer2@gmail.com>
2025-09-27 23:54:39 -0400

    Initial

--- backend/src/main/java/com/quiverly/backend/service/UserService.java
+++ backend/src/main/java/com/quiverly/backend/service/UserService.java
@@ -1,4 +1,29 @@
 package com.quiverly.backend.service;
 
+import com.quiverly.backend.model.User;
+import com.quiverly.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public List<User> getUsers(){ return userRepository.findAll();}
+
+    public void addNewUser(User user){
+        Optional<User> userOptional = userRepository.findUserByEmail(user.getEmail()); //need to implement findUserByEmail query
+        if (userOptional.isPresent()){
+            throw new IllegalStateException("Email is already taken");
+        }
+        userRepository.save(user);
+        System.out.println(user);
+    }
 }
