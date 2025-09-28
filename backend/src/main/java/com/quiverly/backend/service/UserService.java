8390c9dacc92e0fa991ae5adfe32231e62044383
michael-kudrik <mikethesurfer2@gmail.com>
2025-09-28 14:28:59 -0400

    CRUD working for user

--- backend/src/main/java/com/quiverly/backend/service/UserService.java
+++ backend/src/main/java/com/quiverly/backend/service/UserService.java
@@ -2,10 +2,12 @@ package com.quiverly.backend.service;
 
 import com.quiverly.backend.model.User;
 import com.quiverly.backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -25,5 +27,26 @@ public class UserService {
         }
         userRepository.save(user);
         System.out.println(user);
+    }
+
+    public void deleteUser(Long userId){
+        boolean exists = userRepository.existsById(userId);
+        if(!exists){
+            throw new IllegalStateException("User with id: " + userId + " does not exist!");
+        }
+        userRepository.deleteById(userId);
+    }
+
+    @Transactional
+    public void updateUser(Long userId, String username, String email){
+        User user = userRepository.findById(userId).orElseThrow(()-> new IllegalStateException("User with id: " + userId + " does not exist!"));
+
+        if (username != null && !username.isEmpty() && !Objects.equals(user.getEmail(), email)){
+            Optional<User> userOptional = userRepository.findUserByEmail(email);
+            if (userOptional.isPresent()){
+                throw new IllegalStateException("The email " + email + " is taken!");
+            }
+            user.setEmail(email);
+        }
     }
 }
