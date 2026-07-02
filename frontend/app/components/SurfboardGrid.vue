b2f9c2ae530c14c08693cfd777427ea2390b0892
Michael Kudrik <mikethesurfer2@gmail.com>
2026-07-02 00:11:10 -0400

    start SurfboardGrid.vue implementation, introduce skeleton and error. Minor useSurfboards.ts refactor: id and owner does not need to be sent on post

--- frontend/app/components/SurfboardGrid.vue
+++ frontend/app/components/SurfboardGrid.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-const { getMyBoards } = useSurfboards()
+const {getMyBoards} = useSurfboards()
 
-const { data, status, error, refresh } = await getMyBoards()
+const { data: boardsData, status, error, refresh } = getMyBoards()
+
 </script>
 
 <template>
-<p>{{data}}</p>
+  <div v-if="status === 'pending'">
+   <div class="flex gap-4">
+     <div v-for="n in 6"
+          :key="n"
+          class="skeleton h-32 w-32">
+     </div>
+   </div>
+  </div>
+  <div v-if="status === 'error'">
+    <div role="alert" class="alert alert-error">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+      <span>Error! Task failed successfully.</span>
+    </div>
+  </div>
+<p>{{boardsData}}</p>
 </template>
 
 <style scoped>
--- frontend/app/composables/useSurfboards.ts
+++ frontend/app/composables/useSurfboards.ts
@@ -18,20 +18,23 @@ export interface SurfBoard {
   purchasedAt: string
 }
 
+export type CreateSurfboardInput = Omit<SurfBoard, 'id' | 'owner'>
+
 export const useSurfboards = () => {
-  const api = useApi() // unauthenticated $fetch
+  const api = useApi() // authenticated $fetch
 
   // fetching boards uses the useApiFetch composable (automatically attaches Authorization headers)
   const getMyBoards = () => {
 
     // return the result of useApiFetch. This should return a Res, Pending, and Error object
     return useApiFetch<SurfBoard[]>('/api/v1/surfboard/my-boards', {
-      key: 'my-boards'
+      key: 'my-boards',
+      default: () => []
     })
   }
 
   // api calls for write operations.
-  const addBoard = async (boardData: Partial<SurfBoard>) => {
+  const addBoard = async (boardData: CreateSurfboardInput) => {
 
     // uses custom $fetch to handle the auth headers
     // tells the server to do something
